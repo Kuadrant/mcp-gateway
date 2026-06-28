86e759439a1026de639a7c8388da379440d87f56
rogueslasher <aniketpandey25092005@gmail.com>
2026-06-28 15:36:48 +0530

    fix: address coderabbit review comments
    Signed-off-by: rogueslasher <aniketpandey25092005@gmail.com>

--- internal/broker/upstream/manager_test.go
+++ internal/broker/upstream/manager_test.go
@@ -1109,8 +1109,8 @@ func TestServerToolsManagement(t *testing.T) {
 			// Verify serverTools
 			manager.toolsLock.RLock()
 			serverToolNames := make([]string, len(manager.tools.serverItems))
-			for i, st := range manager.tools.serverItems {
-				serverToolNames[i] = st.Tool.Name
+			for i := range manager.tools.serverItems {
+				serverToolNames[i] = manager.tools.serverItems[i].Tool.Name
 			}
 			manager.toolsLock.RUnlock()
 
